6b34df6fe396c570e93e3490f355db91e98d719f
Jordi <hola@jordimoreno.cat>
2025-11-26 22:05:59 +0100

    minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
+const CleanCSS = require("clean-css");
+const Terser = require("terser");
+
 module.exports = function(eleventyConfig) {
+
+  // Fonts
   eleventyConfig.addPassthroughCopy({ "src/assets/fonts": "assets/fonts" });
+
+  // Watch
   eleventyConfig.addWatchTarget("src/assets/css");
   eleventyConfig.addWatchTarget("src/assets/js");
 
+  // Filters
+  eleventyConfig.addFilter("cssmin", code =>
+    new CleanCSS({}).minify(code).styles
+  );
+
+  eleventyConfig.addFilter("jsmin", code => {
+    const minified = Terser.minify(code);
+    return minified.error ? code : minified.code;
+  });
+
+  // Options
   return {
     dir: {
       input: "src",
