a433fbca34ccf3d72270ccaac7851eb60022ea6f
Jordi <hola@jordimoreno.cat>
2025-11-26 20:14:08 +0100

    Fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,13 @@
-module.exports.config = {
-  dir: {
-    input: "_views",
-    includes: "_includes",
-    layouts: "_layouts",
-  }
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "_views/assets/fonts": "assets/fonts" });
+  eleventyConfig.addWatchTarget("_views/assets/css");
+  eleventyConfig.addWatchTarget("_views/assets/js");
+
+  return {
+    dir: {
+      input: "_views",
+      includes: "_includes",
+      layouts: "_layouts"
+    }
+  };
 };
