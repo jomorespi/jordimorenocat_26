4775564872a8bfb7d9a06593294fadc3559556cc
Jordi Moreno <hola@jordimoreno.cat>
2026-03-23 22:53:50 +0100

    Font Switcher (wip)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,9 @@
 import { transitionVars } from "./inc/transition-vars.js";
 import { themeSwitcher } from "./inc/theme-switcher.js";
+import { fontSwitcher } from "./inc/font-switcher.js";
 
 window.addEventListener("DOMContentLoaded", function() {
     transitionVars();
     themeSwitcher();
+    fontSwitcher();
 });
