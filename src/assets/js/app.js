21f68d9c638c1bc954738885b74a49fc926b0805
Jordi Moreno <hola@jordimoreno.cat>
2026-03-23 21:36:00 +0100

    Theme toggler icon and transitions

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,7 @@
+import { transitionVars } from "./inc/transition-vars.js";
 import { themeSwitcher } from "./inc/theme-switcher.js";
 
 window.addEventListener("DOMContentLoaded", function() {
+    transitionVars();
     themeSwitcher();
 });
---
+++ src/assets/js/inc/transition-vars.js
@@ -0,0 +1,8 @@
+export function transitionVars() {
+    const root = document.documentElement;
+
+    setTimeout(() => {
+        root.style.setProperty("--transition-duration", '.3s');
+        root.style.setProperty("--transition-function", 'ease-in-out');
+    }, 150);
+}
