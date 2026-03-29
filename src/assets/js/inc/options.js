2e503bf87f983b9967607b049a6e9b024dd45328
Jordi Moreno <hola@jordimoreno.cat>
2026-03-29 21:27:52 +0200

    Options toggle

--- src/assets/js/inc/options.js
+++ src/assets/js/inc/options.js
@@ -2,6 +2,33 @@ const options = () => {
 
     const html = document.documentElement;
 
+    // Submenu Toggle
+    const submenuToggle = () => {
+        const optionBtns = document.querySelectorAll(".options > ul > li > button");
+        const optionWrappers = document.querySelectorAll(".options__submenu");
+
+        if (!optionBtns.length) return;
+
+        optionBtns.forEach(btn => {
+            const wrapper = btn.nextElementSibling;
+
+            btn.addEventListener("click", () => {
+
+                const isVisible = wrapper ? wrapper.classList.contains("options__submenu--visible") : false;
+
+                optionWrappers.forEach(w => {
+                    w.classList.remove("options__submenu--visible");
+                });
+
+                if (wrapper && !isVisible) {
+                    wrapper.classList.add("options__submenu--visible");
+                }
+            });
+
+        });
+    }
+
+
     // Theme Switcher
     const themeSwitcher = () => {
         const themeToggle = document.getElementById("theme-toggle");
@@ -12,7 +39,7 @@ const options = () => {
 
         document.documentElement.setAttribute("data-theme", currentTheme);
 
-        themeToggle.addEventListener("click", function() {
+        themeToggle.addEventListener("click", function () {
             currentTheme = html.getAttribute("data-theme") === "light" ? "dark" : "light";
             html.setAttribute("data-theme", currentTheme);
             localStorage.setItem("theme", currentTheme);
@@ -21,18 +48,13 @@ const options = () => {
 
     // Font Switcher
     const fontSwitcher = () => {
-        const fontSwitchToggle = document.getElementById("font-switch");
-
-        if (!fontSwitchToggle) return;
-
         const fontSwitchMenu = document.querySelector('.options__submenu--font');
+
+        if (!fontSwitchMenu) return;
+
         const fontOptions = fontSwitchMenu.querySelectorAll("input[type='radio']");
-
-        fontSwitchToggle.addEventListener("click", function() {
-            fontSwitchMenu.classList.toggle("options__submenu--visible");
-        });
-
         const selectedFont = localStorage.getItem("selectedFont");
+
         if (selectedFont) {
             html.classList.add(`font-${selectedFont}`);
 
@@ -41,11 +63,10 @@ const options = () => {
                     option.checked = true;
                 }
             });
-
         }
 
         fontOptions.forEach(option => {
-            option.addEventListener("change", function() {
+            option.addEventListener("change", function () {
                 const selectedFont = this.value;
                 html.classList.remove("font-sans", "font-serif", "font-mono");
                 html.classList.add(`font-${selectedFont}`);
@@ -55,6 +76,7 @@ const options = () => {
     }
 
     // Initialize Options
+    submenuToggle();
     fontSwitcher();
     themeSwitcher();
 
